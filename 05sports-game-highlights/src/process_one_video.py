3e676868d1ebeb6d9f90bc5b59f8c99f98fecd3d
cynthia <cynthiaawsajira@gmail.com>
2025-01-27 01:37:37 +0300

    feat: Add script to process and upload a single video from S3
    - Added  script to fetch a JSON file from S3.
    - Extracts video URL from the JSON file and downloads the video.
    - Uploads the downloaded video to a specified S3 bucket.
    - Includes error handling and logging for each step of the process.

---
+++ 05sports-game-highlights/src/process_one_video.py
@@ -0,0 +1,41 @@
+import json
+import boto3
+import requests
+
+from io import BytesIO
+
+from config import (
+    S3_BUCKET_NAME,
+    AWS_REGION,
+    INPUT_KEY,
+    OUTPUT_KEY
+)
+
+def process_one_video():
+    try:
+        s3 = boto3.client("s3", region_name=AWS_REGION)
+        print("Fetching JSON file from S3")
+        response = s3.get_object(Bucket=S3_BUCKET_NAME, Key=INPUT_KEY)
+        json_content = response['Body'].read().decode('utf-8')
+        highlights = json.loads(json_content)
+        video_url = highlights["data"][0]["url"]
+        print(f"Processing video URL: {video_url}")
+        print("Downloading video...")
+        video_response = requests.get(video_url)
+        video_response.raise_for_status()
+        print("Uploading video to S3...") 
+
+        s3.put_object(
+            Bucket=S3_BUCKET_NAME,
+            Key=OUTPUT_KEY,
+            Body=video_data,
+            ContentType="video/mp4"
+        )
+        print(f"Video uploaded successfully: s3://{S3_BUCKET_NAME}/{OUTPUT_KEY}")
+        print(f"Vidoe uploaded successfully: s3://{S3_BUCKET_NAME}/{OUTPUT_KEY}")
+
+    except Exception as e:
+        print(f"Error during video processing: {e}")
+
+if __name__ =="__main__":
+    process_one_video()
